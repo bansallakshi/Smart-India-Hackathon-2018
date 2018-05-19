8aceb77d11a9e689b9562fba9237dcec531af777
rohilla21 <kritirohilla567@gmail.com>
2018-05-19 11:03:17 +0530

    Remove all comments from settings.py

--- Portal_FilmShooting/Portal_FilmShooting/settings.py
+++ Portal_FilmShooting/Portal_FilmShooting/settings.py
@@ -48,20 +48,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Portal_FilmShooting.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -78,10 +70,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/2.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -91,9 +79,5 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
